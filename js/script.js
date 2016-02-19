909fc3de51a53626dc73b3569d4c74a0cda189b6
tcasey <trevort.casey@gmail.com>
2016-02-19 00:51:34 -0700

    feat: hover, personal styling, setup js events

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+
+	// $('#imac').hover(function(){
+	//         $(this).find('img').fadeTo(500, 0.5);
+	// 	}, function(){
+	//         $(this).find('img').fadeTo(500, 1);
+	// 	});
+	//
+	// $('#screen-background').click(function() {
+	// 	    $('#imac').find('img').fadeTo(0, 1);
+	//         $("#screen-background").stop().animate({width: 800, height: 533}, 200 );
+	//         }).mouseout(function(){
+	//         $("#screen-background").stop().animate({width: 280, height: 187}, 200 );
+	//     });
+alert('sup');
+
+});
