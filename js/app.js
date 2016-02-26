9ee557cd2930f5149e32f86c507cc2a7f7a0d22e
tcasey <trevort.casey@gmail.com>
2016-02-26 11:58:53 -0700

    feat: no # in url + text center in hex

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 angular.module('myApp', ['ui.router'])
-.config(function($urlRouterProvider, $stateProvider){
+.config(function($urlRouterProvider, $stateProvider, $locationProvider){
 
+	$urlRouterProvider
+		.otherwise('home');
 
 	$stateProvider
 
 	.state('home', {
 			url: '/home',
 			templateUrl: 'templates/home.html',
-			controller: 'homeCtrl',
+			// controller: 'homeCtrl',
 	})
 	.state('bio',{
 			url: '/bio',
 			templateUrl: 'templates/bio.html',
-			controller: 'bioCtrl',
+			// controller: 'bioCtrl',
 	})
 	.state('projects',{
 			url: '/projects',
 			templateUrl: 'templates/projects.html',
-			controller: 'projectsCtrl',
+			// controller: 'projectsCtrl',
 	})
 	.state('skills',{
 			url: '/skills',
 			templateUrl: 'templates/skillset.html',
-			controller: 'skillsCtrl',
+			// controller: 'skillsCtrl',
 	})
-
-	$urlRouterProvider
-		.otherwise('home');
-
-
-
+		$locationProvider.html5Mode(true);
 });
+
+
+
 
 $('#icon-wrap-2').click(function(){
   if ($(this).hasClass('toClose')){
