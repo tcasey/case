234fe854077872a383ace8e6ff80dea0a6fb3066
tcasey <trevort.casey@gmail.com>
2016-08-09 00:05:06 -0600

    bug fix: took out super large svg path

--- main/js/app.js
+++ main/js/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ui.router', 'ngAnimate', 'ngTouch'])
+angular.module('myApp', ['ui.router'])
 
 .config(function($urlRouterProvider, $stateProvider, $locationProvider){
 
--- public/scripts/all.js
+++ public/scripts/all.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ui.router', 'ngAnimate', 'ngTouch'])
+angular.module('myApp', ['ui.router'])
 
 .config(["$urlRouterProvider", "$stateProvider", "$locationProvider", function($urlRouterProvider, $stateProvider, $locationProvider){
 
