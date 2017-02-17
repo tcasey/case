200609509862a68335cade8471c2efb73b591e0a
tcasey <hello@tcasey.me>
2017-02-17 14:46:46 -0700

    :tada: gulp goodies :tada:

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
     styles = require('gulp-sass'),
     concat = require('gulp-concat'),
-    // uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    plumber = require('gulp-plumber'),
+    htmlmin = require('gulp-htmlmin'),
     uglifycss = require('gulp-uglifycss'),
+    minifyCss = require('gulp-minify-css'),
     ngAnnotate = require('gulp-ng-annotate'),
     watcher = gulp.watch(['./main/**/*.js', './main/css/*.scss', './main/**/*.html'], ['default']);
 
@@ -14,30 +18,45 @@ watcher.on('change', function(event) {
 gulp.task('styles', function() {
   return gulp.src('./main/**/*.scss')
     .pipe(styles())
-    // .pipe(uglifycss())
+    .pipe(minifyCss())
+    .pipe(plumber())
     .pipe(concat('style.css'))
+    .pipe(rename({
+      basename : 'style',
+      extname : '.min.css'
+    }))
     .pipe(gulp.dest('./docs/styles'))
 });
 
 gulp.task('javascript', function() {
   return gulp.src(['./main/**/*.js'])
     .pipe(ngAnnotate())
-    // .pipe(uglify())
+    .pipe(plumber())
     .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(rename({
+      basename : 'all',
+      extname : '.min.css'
+    }))
     .pipe(gulp.dest('./docs/scripts'))
 });
 
 gulp.task('vendor', function() {
   return gulp.src(['./node_modules/image-loader-angular/js/image-loader.js'])
     .pipe(ngAnnotate())
-    // .pipe(uglify())
+    .pipe(plumber())
     .pipe(concat('vendor.js'))
+    .pipe(uglify())
+    .pipe(rename({
+      basename : 'vendor',
+      extname : '.min.css'
+    }))
     .pipe(gulp.dest('./docs/scripts'))
 });
 
 gulp.task('html', function() {
   return gulp.src('./main/**/*.html')
-    // .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('./docs/html'))
 });
 
